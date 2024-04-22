90d68afe1ec099937fc14acefecf60da559403b1
mquintana3 <mquintana@itpatagonia.com>
2024-04-22 20:33:35 -0300

    prueba de commit numero 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Hola, bienvenidos a Casa de Campo La Capilla!</h1>
+      <h1>
+        Hola, bienvenidos a Casa de Campo La Capilla! En Castilla, Provincia de
+        Buenos Aires
+      </h1>
     </>
   );
 }
