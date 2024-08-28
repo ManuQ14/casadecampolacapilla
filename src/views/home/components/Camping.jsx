80233807ded4a3e287133d84eb0a395b76b3ccce
ManuQ14 <manuquintanafotos@gmail.com>
2024-08-28 18:01:05 -0300

    prueba de commit casadecampo

--- src/views/home/components/Camping.jsx
+++ src/views/home/components/Camping.jsx
@@ -4,7 +4,7 @@ export const Camping = () => {
   return (
     <>
         <h3>El Camping</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus sunt recusandae odit minus neque nihil deleniti nostrum veniam quaerat quod, nam saepe sint doloribus incidunt porro suscipit harum nesciunt minima?</p>
+        <p>El mejor camping del mundo</p>
     </>
   )
 }
