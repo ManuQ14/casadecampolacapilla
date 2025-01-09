b114c280bfb58557c8040f03c065e25acbedf7fe
ManuQ14 <manuquintanafotos@gmail.com>
2025-01-09 01:55:45 -0300

    cambios en version mobile, pendientes de nuevas implementaciones

--- src/views/home/components/Camping.jsx
+++ src/views/home/components/Camping.jsx
@@ -1,27 +1,46 @@
+import { Link } from "react-router-dom";
+
 import styles from "../styles/home.module.scss";
 import imageCamping from "../../../assets/images/6.jpg";
 
+import line from "../../../assets/icons/subrayServices.svg";
+import chevronRight from "../../../assets/icons/chevronRigth.svg";
+
 export const Camping = () => {
   return (
-    <div className={styles.sectionContainer}>
+    <div className={styles.campingSection}>
       <div className={styles.campingTextContainer}>
-        <h2 className={styles.h2}>El Camping</h2>
+        <div className={styles.containerTitle}>
+          <h2 className={styles.subtitle1}>El Camping</h2>
+          <img
+            src={line}
+            alt="linea subrayadora"
+            className={styles.lineSubrayer}
+          />
+        </div>
         <div className={styles.textCamping}>
           Casa de Campo La Capilla te invita a disfrutar de la tranquilidad y
           los sonidos auténticos de la vida rural. Este es un lugar donde la
           conexión con la naturaleza se vive plenamente, y hacemos todo lo
-          posible para que no te falte nada durante tu estadía. <br /> Ofrecemos
-          un servicio de acampe diseñado para que experimentes el encanto del
-          campo, acompañado del mejor servicio personalizado. <br /> <br />
+          posible para que no te falte nada durante tu estadía. <br />
+          Ofrecemos un servicio de acampe diseñado para que experimentes el
+          encanto del campo, acompañado del mejor servicio personalizado. <br /> <br />
           Además, desde nuestro campito, ubicado al borde del pueblo, podrás
           deleitarte con los amaneceres y atardeceres más hermosos, contemplando
           el horizonte sembrado que se extiende hasta donde alcanza la vista.
+        </div>
+        <div className={styles.linkContainer}>
+          <Link to="/nuestra-historia" className={styles.buttonToOurHistory}>
+            Conocé más de nuestra historia
+          </Link>
+          <img src={chevronRight} alt="" className={styles.chevronRight} />
         </div>
       </div>
       <img
         className={styles.campingImage}
         src={imageCamping}
         alt="Atardecer en la capilla"
+        width="100%"
       />
     </div>
   );
--- src/views/home/components/Header.jsx
+++ src/views/home/components/Header.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { NavBar } from "./NavBar";
 
+import videoPortada from "../../../assets/videos/videoPortada.mp4";
+
 import styles from "../styles/home.module.scss";
 
 export const Header = () => {
@@ -10,10 +12,19 @@ export const Header = () => {
 
       <header className={styles.header}>
         <div className={styles.coverHeader}>
+          <video 
+          autoPlay 
+          loop
+          muted
+          playsInline
+          className={styles.videoPortada}
+          >
+            <source src={videoPortada} type="video/mp4" />
+          </video>
           <div className={styles.boxMobileContainer}>
             <div className={styles.titlesContainer}>
-            <h1>Casa de Campo La Capilla</h1>
-            <h3>Experiencia de acampe rural, vivila</h3>
+              <h1>Casa de Campo La Capilla</h1>
+              <h3>Experiencia de acampe rural, vivila...</h3>
             </div>
             <Link to="/contacto" className={styles.reserveButton}>
               Reservar
