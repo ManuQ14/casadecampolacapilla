dc0e7844e48b081c4f99abe385af71380855acf5
ManuQ14 <manuquintanafotos@gmail.com>
2025-01-10 19:45:31 -0300

    cambios en el camping, avance de carrousel

--- src/views/home/components/Camping.jsx
+++ src/views/home/components/Camping.jsx
@@ -1,17 +1,35 @@
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import styles from "../styles/home.module.scss";
-//import imageCamping from "../../../assets/images/6.jpg";
 
 import line from "../../../assets/icons/subrayServices.svg";
 import chevronRight from "../../../assets/icons/chevronRigth.svg";
 
+import img1 from "../../../assets/images/1.jpg";
+import img2 from "../../../assets/images/2.jpg";
+import img3 from "../../../assets/images/3.jpg";
+import img4 from "../../../assets/images/4.jpg";
+import img5 from "../../../assets/images/5.jpg";
+
+const carrouselImages = [img1, img2, img3, img4, img5];
+
 export const Camping = () => {
   const navigate = useNavigate();
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % carrouselImages.length);
+    }, 3000); // Cambia cada 3 segundos
+    return () => clearInterval(interval);
+  }, []);
 
   const handleToHistory = () => {
     navigate("/nuestra-historia");
   };
+
+  const goToIndex = (index) => setCurrentIndex(index);
 
   return (
     <div className={styles.campingSection} id="camping">
@@ -37,15 +55,37 @@ export const Camping = () => {
           el horizonte sembrado que se extiende hasta donde alcanza la vista.
         </div>
         <div className={styles.conoceMasContainer} onClick={handleToHistory}>
-          <span className={styles.conoceMasText}>Conocé mas de nuestra historia</span>
+          <span className={styles.conoceMasText}>
+            Conocé más de nuestra historia
+          </span>
           <img
             src={chevronRight}
-            alt="Chrevron right"
+            alt="Chevron right"
             className={styles.chevronRight}
           />
         </div>
       </div>
-      <div className={styles.carrouselCamping}></div>
+      <div className={styles.carrouselCamping}>
+        <div
+          className={styles.carouselTrack}
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        >
+          {carrouselImages.map((image, index) => (
+            <img key={index} src={image} alt={`Slide ${index + 1}`} />
+          ))}
+        </div>
+        <div className={styles.carouselIndicators}>
+          {carrouselImages.map((_, index) => (
+            <span
+              key={index}
+              className={`${styles.dot} ${
+                currentIndex === index ? styles.active : ""
+              }`}
+              onClick={() => goToIndex(index)}
+            ></span>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
