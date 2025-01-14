f69b6e2b641bd4b089031b7a2cf287d7cee2c8a6
ManuQ14 <manuquintanafotos@gmail.com>
2025-01-14 12:36:14 -0300

    cambios vercel

--- pages/api/reviews.js
+++ pages/api/reviews.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+//import fetch from 'node-fetch';
 
 let cachedReviews = null;
 let lastFetchTime = 0;
@@ -12,18 +12,18 @@ export default async function handler(req, res) {
 
   try {
     const apiKey = process.env.GOOGLE_API_KEY; // No cambiar, esto funciona en Vercel
-    const placeId = 'ChIJa9a-wxhUuZURWP9hFWNy2Wc';
+    const placeId = "ChIJa9a-wxhUuZURWP9hFWNy2Wc";
     const url = `https://maps.googleapis.com/maps/api/place/details/json?placeId=${placeId}&fields=reviews&key=${apiKey}`;
 
     const response = await fetch(url);
     const data = await response.json();
 
-    if (data.status === 'OK') {
+    if (data.status === "OK") {
       cachedReviews = data.result.reviews || [];
       lastFetchTime = Date.now();
       res.status(200).json({ reviews: cachedReviews });
     } else {
-      res.status(500).json({ error: 'Error fetching reviews' });
+      res.status(500).json({ error: "Error fetching reviews" });
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
