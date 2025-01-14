6701b40036ed9243cd750201781c048e0d6ed60e
ManuQ14 <manuquintanafotos@gmail.com>
2025-01-14 01:28:47 -0300

    prueba de vercel serviceless

--- src/views/home/components/Opinions.jsx
+++ src/views/home/components/Opinions.jsx
@@ -1,12 +1,31 @@
-//import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/home.module.scss";
-
 import line from "../../../assets/icons/subray.svg";
-
-
+import axios from "axios";
 
 export const Opinions = () => {
- 
+  const [reviews, setReviews] = useState([]);
+  const placeId = "ChIJa9a-wxhUuZURWP9hFWNy2Wc"; // Place ID de "Casa de Campo La Capilla"
+  const vercelFunctionUrl = "/api/reviews"; // Reemplaza con la URL de tu función
+
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const response = await axios.get(vercelFunctionUrl, {
+          params: { placeId: placeId },
+        });
+        if (response.data && response.data.reviews) {
+          setReviews(response.data.reviews);
+        } else {
+          console.warn("No reviews found in the response");
+        }
+      } catch (error) {
+        console.error("Error:", error.message);
+      }
+    };
+
+    fetchReviews();
+  }, [vercelFunctionUrl, placeId]);
 
   return (
     <div className={styles.sectionOpiniones}>
@@ -18,8 +37,22 @@ export const Opinions = () => {
           className={styles.line}
         />
       </div>
-     
-   
+      <div>
+        {reviews.length > 0 ? (
+          reviews.map((review, index) => (
+            <div key={index}>
+              <p>
+                <strong>{review.author_name}</strong> - {review.rating}{" "}
+                estrellas
+              </p>
+              <p>{review.text}</p>
+              <hr />
+            </div>
+          ))
+        ) : (
+          <p>Cargando opiniones...</p>
+        )}
+      </div>
     </div>
   );
 };
