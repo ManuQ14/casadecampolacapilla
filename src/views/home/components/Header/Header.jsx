13d6faa8aa077bd38bb1b203a3f8db9a97627b64
ManuQ14 <manuquintanafotos@gmail.com>
2025-02-14 02:00:56 -0300

    correcciones menores de diseño

--- src/views/home/components/Header/Header.jsx
+++ src/views/home/components/Header/Header.jsx
@@ -13,7 +13,8 @@ import youtubeIcon from "../../../../assets/icons/iconsBurguerMenu/youTube.svg";
 import tikTokIcon from "../../../../assets/icons/iconsBurguerMenu/tikTok.svg";
 import facebookIcon from "../../../../assets/icons/iconsBurguerMenu/facebook.svg";
 
-import CapillaLogo from "../../../../assets/icons/CapillaLogoPng.png";
+//import CapillaLogo from "../../../../assets/icons/CapillaLogoPng.png";
+import CapillaLogo from "../../../../assets/icons/iconsBurguerMenu/LogoCapillaSVGMenuBurguer.svg";
 
 export const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
